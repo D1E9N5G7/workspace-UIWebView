c4957c2dd4eea1592180107fcb013dc6a8fd57e7
DENG <75176097@qq.com>
2019-04-26 00:07:15 +0800

    1

--- UIWebView/ViewController.swift
+++ UIWebView/ViewController.swift
@@ -13,12 +13,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let mWKWebView = WKWebView(frame: self.view.frame)
+        
+        let width = self.view.frame.width
+        let height = self.view.frame.height
+        
+        //load website
+        let mWKWebView = WKWebView(frame: CGRect(x: 0, y: 0, width: width, height: height/2))
         let url = URL(string: "https://www.baidu.com")
         let mURLRequest = URLRequest(url: url!)
         mWKWebView.load(mURLRequest)
         self.view.addSubview(mWKWebView)
+        
+        // load local html
+        let nWKWebView = WKWebView(frame: CGRect(x: 0, y: height/2, width: width, height: height/2))
+        let html = "<html><head></head><body><h1>this is html source</h1></body></html>"
+        nWKWebView.loadHTMLString(html, baseURL: nil)
+        self.view.addSubview(nWKWebView)
         
     }
 
